0ea40f6c440b6adb7cf1c935aa0f352fa9eb646a
Maciej Koscielski <maciej.koscielski@capgemini.com>
2018-09-11 17:07:11 +0200

    example

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,29 @@
 #include <Arduino.h>
 
+const int PIN_SOIL_HUMIDITY = A0;
 const int PIN_LED = D1;
+
+const int MINIMAL_HUMIDITY = 45;
+int soilHumidity = 0;
 
 void setup()
 {
+    Serial.begin(9600);
+    
     pinMode(PIN_LED, OUTPUT);
     digitalWrite(PIN_LED, LOW);
 }
 
 void loop()
 {
-    digitalWrite(PIN_LED, HIGH);
-    delay(1000);
-    digitalWrite(PIN_LED, LOW);
-    delay(1000);
+    soilHumidity = analogRead(PIN_SOIL_HUMIDITY);
+    soilHumidity = map(soilHumidity, 0, 1024, 100, 0);
+    if(soilHumidity < MINIMAL_HUMIDITY){
+        digitalWrite(PIN_LED, HIGH);
+    } else {
+        digitalWrite(PIN_LED, LOW);
+    }
+    Serial.print("Humidity: ");
+    Serial.println(soilHumidity);
+    delay(500);
 }
