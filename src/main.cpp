6ae17a989ef0aef7aff6acadd110c0f9e550583a
Maciej Koscielski <maciej.koscielski@capgemini.com>
2018-09-11 16:44:33 +0200

    Example

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 #include <Arduino.h>
 
+const int PIN_LED = D1;
 
-void setup() {
-
+void setup()
+{
+    pinMode(PIN_LED, OUTPUT);
+    digitalWrite(PIN_LED, LOW);
 }
 
-
-void loop() {
-
+void loop()
+{
+    digitalWrite(PIN_LED, HIGH);
+    delay(1000);
+    digitalWrite(PIN_LED, LOW);
+    delay(1000);
 }
